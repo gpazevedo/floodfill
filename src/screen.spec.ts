0a50f9193855853a4a184881ce7d1118b06612ba
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2021-06-07 15:13:36 -0300

    Screen

--- src/screen.spec.ts
+++ src/screen.spec.ts
@@ -1,75 +1,75 @@
 import { Position, Screen } from '@/Screen';
 
 describe('Screen', () => {
-  // describe('Screen construction', () => {
-  //   it('should recognize equal screens', () => {
-  //     expect(JSON.stringify(new Screen([[1], [5]]))).toEqual(JSON.stringify(new Screen([[1], [5]])));
-  //   });
-  //   it('should recognize different screens', () => {
-  //     expect(JSON.stringify(new Screen([[1], [5]]))).not.toEqual(JSON.stringify(new Screen([[1], [7]])));
-  //   });
-  // });
+  describe('Screen construction', () => {
+    it('should recognize equal screens', () => {
+      expect(JSON.stringify(new Screen([[1], [5]]))).toEqual(JSON.stringify(new Screen([[1], [5]])));
+    });
+    it('should recognize different screens', () => {
+      expect(JSON.stringify(new Screen([[1], [5]]))).not.toEqual(JSON.stringify(new Screen([[1], [7]])));
+    });
+  });
 
-  // describe('inside Screen', () => {
-  //   const matrix = [
-  //     [1, 3],
-  //     [3, 3],
-  //     [5, 2],
-  //   ];
-  //   const screen = new Screen(matrix);
+  describe('inside Screen', () => {
+    const matrix = [
+      [1, 3],
+      [3, 3],
+      [5, 2],
+    ];
+    const screen = new Screen(matrix);
 
-  //   it('should identify inside points 2,1', () => {
-  //     expect(screen.inside({ line: 2, column: 1 })).toBe(true);
-  //   });
+    it('should identify inside points 2,1', () => {
+      expect(screen.inside({ line: 2, column: 1 })).toBe(true);
+    });
 
-  //   it('should identify inside points 0,0', () => {
-  //     expect(screen.inside({ line: 0, column: 0 })).toBe(true);
-  //   });
+    it('should identify inside points 0,0', () => {
+      expect(screen.inside({ line: 0, column: 0 })).toBe(true);
+    });
 
-  //   it('should identify OUTside points 3,1', () => {
-  //     expect(screen.inside({ line: 3, column: 1 })).toBe(false);
-  //   });
+    it('should identify OUTside points 3,1', () => {
+      expect(screen.inside({ line: 3, column: 1 })).toBe(false);
+    });
 
-  //   it('should identify OUTside points -31', () => {
-  //     expect(screen.inside({ line: -3, column: 1 })).toBe(false);
-  //   });
-  //   it('should identify OUTside points 32', () => {
-  //     expect(screen.inside({ line: 3, column: 2 })).toBe(false);
-  //   });
-  // });
+    it('should identify OUTside points -31', () => {
+      expect(screen.inside({ line: -3, column: 1 })).toBe(false);
+    });
+    it('should identify OUTside points 32', () => {
+      expect(screen.inside({ line: 3, column: 2 })).toBe(false);
+    });
+  });
 
-  // describe('getColor', () => {
-  //   const matrix = [
-  //     [1, 3],
-  //     [3, 3],
-  //     [5, 2],
-  //   ];
-  //   const screen = new Screen(matrix);
+  describe('getColor', () => {
+    const matrix = [
+      [1, 3],
+      [3, 3],
+      [5, 2],
+    ];
+    const screen = new Screen(matrix);
 
-  //   it('should work on a correct position', () => {
-  //     expect(screen.getColor({ line: 0, column: 1 })).toBe(3);
-  //   });
-  //   it('should work on a incorrect position', () => {
-  //     expect(screen.getColor({ line: 0, column: -11 })).toBe(-1);
-  //   });
-  // });
+    it('should work on a correct position', () => {
+      expect(screen.getColor({ line: 0, column: 1 })).toBe(3);
+    });
+    it('should work on a incorrect position', () => {
+      expect(screen.getColor({ line: 0, column: -11 })).toBe(-1);
+    });
+  });
 
-  // describe('setColor', () => {
-  //   const matrix = [
-  //     [1, 3],
-  //     [3, 3],
-  //     [5, 2],
-  //   ];
-  //   const screen = new Screen(matrix);
+  describe('setColor', () => {
+    const matrix = [
+      [1, 3],
+      [3, 3],
+      [5, 2],
+    ];
+    const screen = new Screen(matrix);
 
-  //   it('should work on a correct position', () => {
-  //     expect(screen.setColor({ line: 0, column: 1 }, 5)).toBe(true);
-  //     expect(screen.getColor({ line: 0, column: 1 })).toBe(5);
-  //   });
-  //   it('should work on a incorrect position', () => {
-  //     expect(screen.setColor({ line: 0, column: -11 }, 5)).toBe(false);
-  //   });
-  // });
+    it('should work on a correct position', () => {
+      expect(screen.setColor({ line: 0, column: 1 }, 5)).toBe(true);
+      expect(screen.getColor({ line: 0, column: 1 })).toBe(5);
+    });
+    it('should work on a incorrect position', () => {
+      expect(screen.setColor({ line: 0, column: -11 }, 5)).toBe(false);
+    });
+  });
 
   describe('neighbors', () => {
     const matrix1 = [
